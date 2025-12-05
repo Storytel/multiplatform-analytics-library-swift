4f55d09d5d0f6b29bac72e6706b5b443d2018c4b
Mario Ferreira Vilanova <mario.ferreira.vilanova@gmail.com>
2025-12-05 11:47:14 +0100

    first push

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://github.com/Storytel/multiplatform-analytics-library-swift/releases/download/0.0.1/kotlin.xcframework.zip",
+         checksum:"0c156259083e932899e904facac24b8c7769e3b0bd89c7b34c39c407645eec30")
+   ]
+)
