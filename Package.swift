2801bfed89111c72f9bce20b02d067f3940856af
Erik L. Underbjerg <erik@underbjerg.com>
2025-12-05 15:08:51 +0100

    Update MultiplatformAnalytics to version 0.0.6

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "MultiplatformAnalytics",
-         url: "https://github.com/Storytel/multiplatform-analytics-library-swift/releases/download/0.0.5/MultiplatformAnalytics.xcframework.zip",
-         checksum:"49adc5da8c1894ccdde6d419cf13937f60e4b1817ba6202789cf4b2bb005701e")
+         url: "https://github.com/Storytel/multiplatform-analytics-library-swift/releases/download/0.0.6/MultiplatformAnalytics.xcframework.zip",
+         checksum:"52bd565ddc654c12e18ae15c28fadaa4e8abefcf344c1383b4171a962df30625")
    ]
 )
